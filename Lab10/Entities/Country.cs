da8ed2020d705775aec959b5579d3933f5f88ed8
Карина Угрюмова <ugryumova31.12.2000@gmail.com>
2021-05-26 21:21:10 +0700

    Сделала анонимный делегат (не знаю так или нет, но что-то сделала).

---
+++ Lab10/Entities/Country.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10
+{
+    abstract class Country
+    {
+        string name;
+        string capital;
+        string ruler;
+        string language;
+        string moneyunit;
+        int population;
+        double territory;
+        int yearoffoundation;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string Capital
+        {
+            get
+            {
+                return this.capital;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string Ruler
+        {
+            get
+            {
+                return this.ruler;
+            }
+            set
+            {
+                this.ruler = value;
+            }
+        }
+        public string Language
+        {
+            get
+            {
+                return this.language;
+            }
+        }
+        public string Moneyunit
+        {
+            get
+            {
+                return this.moneyunit;
+            }
+        }
+        public int Population
+        {
+            get
+            {
+                return this.population;
+            }
+            set
+            {
+                this.population = value;
+            }
+        }
+        public double Territory
+        {
+            get
+            {
+                return this.territory;
+            }
+            set
+            {
+                this.territory = value;
+            }
+        }
+        public int Yearoffoundation
+        {
+            get
+            {
+                return this.yearoffoundation;
+            }
+        }
+        public Country()
+        {
+
+        }
+        public Country(string n, string c, string r, string l, string m, int y)
+        {
+            this.name = n;
+            this.capital = c;
+            this.ruler = r;
+            this.language = l;
+            this.moneyunit = m;
+            this.yearoffoundation = y;
+        }
+
+        abstract public double CalculateTerritory();
+        abstract public int CalculatePopulation();
+        public override string ToString()
+        {
+
+            return String.Format("\nНазвание страны: {0};\nСтолица: {1};\nПравитель: {2};\nЯзык: {3};\nДенежная единица: {4};\nНаселение: {5} чел.;\nТерритория: {6}км^2;\nГод основания: {7};\n", this.name, this.capital, this.ruler, this.language,
+                this.moneyunit, this.population, this.territory, this.yearoffoundation);
+        }
+        public virtual void PrintCard()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    public delegate void CountryDel();// для анонимного делегата
+    public delegate int SumTerritory(int a, int b);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //применение анонимных методов
+            Console.WriteLine("Информацию какой страны вы желаете увидеть? \nЕсли России, то введите цифру: '1' \nЕсли Испонии, то введите цифру: '2'");
+
+            Console.Write("Ваша цифра: ");
+
+            int a = int.Parse(Console.ReadLine());
+
+            CountryDel cd = new CountryDel(delegate ()
+             {
+                 switch(a)
+                {
+                     case 1:
+                         Entities.CountryFederation fed1 = new Entities.CountryFederation("Российская Федерация", "Москва", "Путин Владимир Владимирович", "Русский язык", "Российский рубль", 1991);
+                         Console.WriteLine("\n{0}", fed1);
+                         break;
+                     case 2:
+                         Entities.CountryUnitary unt1 = new Entities.CountryUnitary("Испания", "Мадрид", "Филипп VI", "Испанский язык", "Евро", 47431256, 505990, 1479);
+                         Console.WriteLine("\n{0}", unt1);
+                         break;
+                     default:
+                         Console.WriteLine("Вы выбрали не правильный вариант ответа!");
+                         break;
+                         
+                 }
+             });
+            
+            cd();
+            Console.ReadLine();
+        }
+    }
+}
